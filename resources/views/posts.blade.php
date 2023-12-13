a0ee1fa46ac1a23254e6dfa9359b4f51f51a087f
ebr-salem <ebrahmedsalem03@gmail.com>
2023-12-13 13:03:42 +0200

    add the static posts and show one post.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Posts</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <div class="container">
+        <article>
+            <h1><a href="/posts/first-post">First Post</a></h1>
+
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, non impedit corrupti molestias
+                accusamus
+                magnam. Impedit possimus consequatur quibusdam ea blanditiis ipsam ipsum voluptas cum autem! Animi dicta
+                repellendus, amet itaque harum eos iusto voluptatem.
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/posts/second-post">Second Post</a></h1>
+
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, non impedit corrupti molestias
+                accusamus
+                magnam. Impedit possimus consequatur quibusdam ea blanditiis ipsam ipsum voluptas cum autem! Animi dicta
+                repellendus, amet itaque harum eos iusto voluptatem.
+            </p>
+        </article>
+
+        <article>
+            <h1><a href="/posts/third-post">Third Post</a></h1>
+
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis, non impedit corrupti molestias
+                accusamus
+                magnam. Impedit possimus consequatur quibusdam ea blanditiis ipsam ipsum voluptas cum autem! Animi dicta
+                repellendus, amet itaque harum eos iusto voluptatem.
+            </p>
+        </article>
+    </div>
+</body>
+
+</html>
