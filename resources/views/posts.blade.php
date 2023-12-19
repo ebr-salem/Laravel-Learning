3e727c1e7c5eaa97ff05cfb4b38db06dc759e402
ebr-salem <ebrahmedsalem03@gmail.com>
2023-12-19 21:58:43 +0200

    Route model binding

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
             @foreach ($posts as $post)
                 <article>
                     <h1>
-                        <a href="/posts/{{ $post->id }}">{{ $loop->index + 1 }} - {{ $post->title }}</a>
+                        <a href="/posts/{{ $post->slug }}">{{ $loop->index + 1 }} - {{ $post->title }}</a>
                     </h1>
 
                     <p>{{ $post->body }}</p>
