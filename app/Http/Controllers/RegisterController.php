093e1e9c53c700bdaf81e996b011e897f9cdd1b3
ebr-salem <ebrahmedsalem03@gmail.com>
2024-01-03 17:12:16 +0200

    Registeration

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:4|max:255',
+            'email' => 'required|max:255',
+            'password' => 'required|min:7|max:255',
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/');
+    }
+}
---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,51 @@
+<x-layout>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register</h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" method="POST">
+                @csrf
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
+                        name
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name"
+                        value="{{ old('name') }}" required>
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
+                        username
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username"
+                        value="{{ old('username') }}" required>
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
+                        email
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email"
+                        value="{{ old('email') }}" required>
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
+                        password
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"
+                        required>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                        in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-layout>
