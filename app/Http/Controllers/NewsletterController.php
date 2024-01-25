b4d5fd7e14a39804ac4ddf799ab075d422700b3b
ebr-salem <ebrahmedsalem03@gmail.com>
2024-01-25 21:51:21 +0200

    Newsletter

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Newsletter;
+
+class NewsletterController extends Controller
+{
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email' => 'required|email']);
+
+        try {
+            $newsletter->subscribe(request('email'));
+        } catch (\Exception $e) {
+            throw \Illuminate\Validation\ValidationException::withMessages([
+                'email' => "Your email will not be added to the newsletter's list."
+            ]);
+        }
+
+        return redirect()->back()->withSuccess("You're no in newsletter!");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\NewsletterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comment/store', [CommentController::class, 'store'])->middleware('auth');
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
